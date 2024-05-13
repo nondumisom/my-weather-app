fd08dec25f93266a5731e16d157aa000e765a45e
nondumisom <malazanondumiso@gmail.com>
2024-05-13 13:41:36 +0200

    Added search functionality

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+
+  //   console.log(response.data.temperature.current);
+}
+
+function searchCity(city) {
+  let apiKey = "ctbf5904743750138d7o2a6ce12b7aaf";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  //   console.log(searchInput.value);
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
-}
 
+  searchCity(searchInput.value);
+}
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Paris");
